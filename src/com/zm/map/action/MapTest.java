3469928e3c7a9a4ca72f21cf9f7947b7237aa6f5
zhangmin <zhangkaisx@126.com>
2016-06-30 10:50:38 +0800

    Map排序方法添加

--- src/com/zm/map/action/MapTest.java
+++ src/com/zm/map/action/MapTest.java
@@ -1,7 +1,11 @@
 package com.zm.map.action;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -27,15 +31,15 @@ public class MapTest {
 		while (it.hasNext()) {
 			Map.Entry p = (Entry) it.next();
 			System.out.println("entrySet Map集合循环1："+p.getKey()+":"+p.getValue());
-			System.out.println("===============================================");
 		}
+		System.out.println("===============================================");
 		
 		for (Entry<String, String> entry : map.entrySet()) {
 			String key = entry.getKey();
 			String value =entry.getValue();
 			System.out.println("entrySet Map集合循环2：" + key + ":" + value);
-			System.out.println("===============================================");
 		}
+		System.out.println("===============================================");
 		
 		
 		//keySet 遍历Map集合
@@ -44,13 +48,27 @@ public class MapTest {
 		   String key = iter.next();  
 		   String value = map.get(key);  
 		   System.out.println("keySet Map集合循环1："+key+":"+value);
-		   System.out.println("===============================================");
 		}
+		System.out.println("===============================================");
 		
 		for (String key : map.keySet()) {
 			 String value = map.get(key);  
 			 System.out.println("keySet Map集合循环2："+key+":"+value);
-			 System.out.println("===============================================");
+		}
+		System.out.println("===============================================");
+		
+		//Map排序
+		List<Map.Entry<String, String>> itSort = new ArrayList<Map.Entry<String,String>>(map.entrySet());
+		//排序
+		Collections.sort(itSort, new Comparator<Map.Entry<String, String>>() {   
+		    public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2) {   
+		    	//以key排序
+		        return (o1.getKey()).toString().compareTo(o2.getKey());
+		    }
+		}); 
+		for (int i = 0; i < itSort.size(); i++) {
+			Map.Entry<String, String> m = (Entry<String, String>) itSort.get(i);
+			System.out.println(m.getKey()+":"+m.getValue());
 		}
 	}
 
